60f1f6698beb03a10a90bbc0e1adf3f591ffa330
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-05-16 18:37:02 -0400

    comitting changes to routes

--- controllers/api/Job_Poster.js
+++ controllers/api/Job_Poster.js
@@ -3,12 +3,22 @@ const { Job_Poster } = require("../../models");
 const withAuth = require("../../utils/auth");
 router.get("/", (req, res) => {
   Job_Poster.findOne({})
-    .then((Job_Poster) => res.json(Job_Apply))
+    .then((Job_Poster) => res.json(Job_Poster))
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
+router.put("/:id", async (req, res) => {
+  // update a category by its `id` value
+  try {
+    const jobPost = await Job_Poster.update(req.body, {
+      where: { id: req.params.id },
+    });
+    res.status(200).json(jobPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-module.exports = router;
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const Job_ApplyRoutes = require("./api/Job_Apply");
 const Job_PosterRoutes = require("./api/Job_Poster");
-const Job_SeekerRoutes = require("./api/Job_Seeker");
-const Jobs = require("./Jobs");
+const User = require("./api/User");
+const Jobs = require("./api/Jobs");
 router.use("/Job_Apply", Job_ApplyRoutes);
 router.use("/Job_Poster", Job_PosterRoutes);
-router.use("/Job_Seeker", Job_SeekerRoutes);
+router.use("/User", UserRoutes);
 router.use("/Jobs", Jobs);
 router.use((res, req) => {
   res.statusCode(404).end();
