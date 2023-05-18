b090a6c7628721ac0dec38e1a2e32de2dbc451f6
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-05-18 19:37:51 -0400

    commiting changes to job apply

--- controllers/api/Job_Apply.js
+++ controllers/api/Job_Apply.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
-const { Job_Apply } = require("../../Models");
+const { Job_Apply, Job_Apply } = require("../../models");
 const withAuth = require("../../utils/auth");
 
+//Find all jobs that you can apply to
 router.get("/", (req, res) => {
   Job_Apply.findAll({})
     .then((Job_Apply) => res.json(Job_Apply))
@@ -11,4 +12,35 @@ router.get("/", (req, res) => {
     });
 });
 
+
+router.put("/:id", async (req, res) => {
+  // update a Job by by its `id` value
+  try {
+    const JobApply = await Job_Apply.update(req.body, {
+      where: { id: req.params.id },
+    });
+    res.status(200).json(JobApply);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+router.delete("/:id", withAuth, (req, res) => {
+  Job_Apply.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }) .then((Job_Apply => {
+    if (!Job_Apply) {
+      res.status(404).json({ message: "No Job Apply found with this id" });
+      return;
+    }
+    res.json(Job_Apply);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+
+
+});
 module.exports = router;
