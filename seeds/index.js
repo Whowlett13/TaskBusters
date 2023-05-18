95a142aaa6df87f5c3faa7ca145cc90df291660c
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-05-18 19:22:09 -0400

    adding index.js file to seeds folder

---
+++ seeds/index.js
@@ -0,0 +1,15 @@
+const sequelize = require("../config/connection");
+const seedsUser = require("./User");
+const seedJobApply = require("./Job_Apply");
+const seedJobs = require("./Jobs");
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log("\n----- DATABASE SYNCED -----\n");
+  await seedJobApply();
+  console.log("\n----- CATEGORIES SEEDED -----\n");
+
+  process.exit(0);
+};
+
+seedAll();
