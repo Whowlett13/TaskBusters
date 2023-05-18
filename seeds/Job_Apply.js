537b026c8788cb53298167ce55732d368fe8d6ad
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-05-18 19:39:58 -0400

    adding job_Apply file data'

--- seeds/Job_Apply.js
+++ seeds/Job_Apply.js
@@ -1,14 +1,18 @@
-const Job_Apply = require("../models/Job_Apply");
-const sequelize = require("../config/connection");
+const { Job_Apply } = require("../models");
 
-const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
-  const Job_ApplyData = [
-    { job_id: 1, user_id: 1, job_id: 1, comment_text: "I will do the job" },
-  ];
+const jobApplyData = [
+  {
+    job_id: 1,
+    user_id: 1,
+    comment_text: "I will do this job",
+  },
+  {
+    job_id: 1,
+    user_id: 1,
+    comment_text: "I will do this job NOW",
+  },
+];
 
-  Job_Apply.bulkCreate(Job_ApplyData);
-  process.exit(0);
-};
+const seedsJobApply = () => Job_Apply.bulkCreate(jobApplyData);
 
-seedDatabase();
+module.exports = seedsJobApply;
