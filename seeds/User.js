fa6d0863a7542536d0a7f4df93161b3670b596c1
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-05-18 21:28:18 -0400

    commting working seeds

--- seeds/User.js
+++ seeds/User.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-const User = require("../models/User");
+const User = require("../models");
 const sequelize = require("../config/connection");
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -15,21 +14,6 @@ const seedDatabase = async () => {
       password: "hello",
     },
   ];
-=======
-const User = require("../models");
-const UserData = [
-  {
-    first_name: "Wyatt",
-    last_name: "Howlett",
-    username: "Whowlett13",
-    linkedin: "hello",
-    contact_number: 613,
-    date_of_birth: 1999 - 03 - 28,
-    email: "wyatt@gmail.com",
-    password: "hello",
-  },
-];
->>>>>>> 94f3c0f (.)
 
   User.bulkCreate(UserData);
   process.exit(0);
