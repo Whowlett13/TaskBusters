4e6ecc91a4c4f7f97626144ac59a836c47ffdc98
Joshua Sinclair <josh.r.sinclair@gmail.com>
2023-05-18 21:08:19 -0400

    Changes

--- seeds/User.js
+++ seeds/User.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const User = require("../models/User");
 const sequelize = require("../config/connection");
 const seedDatabase = async () => {
@@ -15,21 +14,6 @@ const seedDatabase = async () => {
       password: "hello",
     },
   ];
-=======
-const User = require("../models");
-const UserData = [
-  {
-    first_name: "Wyatt",
-    last_name: "Howlett",
-    username: "Whowlett13",
-    linkedin: "hello",
-    contact_number: 613,
-    date_of_birth: 1999 - 03 - 28,
-    email: "wyatt@gmail.com",
-    password: "hello",
-  },
-];
->>>>>>> 94f3c0f (.)
 
   User.bulkCreate(UserData);
   process.exit(0);
