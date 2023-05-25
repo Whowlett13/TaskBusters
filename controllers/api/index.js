e50078ae9d29ece2d5f97b3a24a255391646609c
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-05-25 16:22:20 -0400

    connect to jawsdb

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
 const userRoutes = require("./User");
-const jobsRoutes = require("./jobs");
+const jobsRoutes = require("./Jobs");
 
 router.use("/User", userRoutes);
-router.use("/jobs", jobsRoutes);
+router.use("/Jobs", jobsRoutes);
 
 module.exports = router;
