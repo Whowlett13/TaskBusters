ba64246a42e475fec3eb616bebd6cf343908043e
Joshua Sinclair <josh.r.sinclair@gmail.com>
2023-05-25 15:12:05 -0400

    fixed homepage render

--- seeds/Jobs.js
+++ seeds/Jobs.js
@@ -10,6 +10,16 @@ const jobsData = [
     hourly_wage: 40,
     job_duration: 1,
   },
+  {
+    user_id: 1,
+    job_title: "General labour",
+    job_description:
+      "help around the house",
+    job_location: "Ottawa",
+    job_date: 2023 - 10 - 11,
+    hourly_wage: 20,
+    job_duration: 8,
+  },
 ];
 const seedJobs = () => Jobs.bulkCreate(jobsData);
 module.exports = seedJobs;
